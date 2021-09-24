5657e826c16f804cb43c93ac53c1d63c303182ac
Zhihan <zhao.zhiha@husky.neu.edu>
2021-09-24 12:17:17 -0700

    Initial Commit

---
+++ FruitsShowByTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FruitsShowByTableView
+//
+//  Created by 赵芷涵 on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
